101e417f2012c9fc12407cefe6b05d536a416c64
Cecilia Ha <hamytrinh9a1@gmail.com>
2023-10-26 00:31:38 +0700

    login success done

--- src/data/login.data.js
+++ src/data/login.data.js
@@ -1,18 +1,17 @@
-[
-    {
+export const LOGIN_SUCCESS = {
         "email": "hacecilia2023@gmail.com",
-        "password": "A123456!"
-    },
-    {
+        "password": "A123456!",
+        "accountName": "Cecilia Ha",
+}
+export const LOGIN_WRONGPASS = {
         "email": "test@gmail.com",
         "password": "A123456!"
-    },
-    {
+}
+export const LOGIN_EMPTYEMAIL = {
         "email": "",
         "password": "A123456!"
-    },
-    {
+}
+export const LOGIN_EMPTYPASSWORD = {
         "email": "A123456!",
         "password": ""
-    }
-]
+}
---
+++ src/pages/common/header.js
@@ -0,0 +1,13 @@
+class Header{
+    get accountName() {
+        //
+        return cy.get(".page-header .logged-in");
+    }
+    get btnArrow() {
+        return cy.get(".page-header button[data-action=\"customer-menu-toggle\"]");
+    }
+    get btnSignOut(){
+        return cy.get("div[aria-hidden='false'] .authorization-link");
+    }
+}
+export const header = new Header();

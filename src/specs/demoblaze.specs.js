7d3811dc95dce32b392eb0d0f00a09f8d62a9daa
Cecilia Ha <hamytrinh9a1@gmail.com>
2023-12-09 09:30:57 +0700

    verify API

--- src/specs/demoblaze.specs.js
+++ src/specs/demoblaze.specs.js
@@ -11,4 +11,34 @@ describe('SR Home Page', () => {
             })
         })
     });
+    it.only('verify card title with API',()=>{
+        cy.visit("https://demoblaze.com/");
+        //get API by using intercept
+        cy.intercept('/entries').as('entries');
+
+        //no data
+        // cy.get('@entries').then(entries => {
+        //     cy.log(JSON.stringify(entries));
+        // })
+        
+        //wait API response data by alias
+        cy.wait('@entries');
+        cy.get('@entries').then(entries => {
+            let apiProductData = entries.response.body.Items;
+            apiProductData = apiProductData.map(item => {
+                return {
+                    itemName: item.title.replace('\n',''),
+                    //string literal
+                    itemPrice: `$${item.price}`
+                }
+            })
+            // cy.log(JSON.stringify(apiProductData));
+            demoblazePage.getAllCardData().then(allItemData => {
+                cy.wrap('').then(()=>{
+                    cy.log(JSON.stringify(allItemData));
+                    expect(allItemData).to.be.deep.eq(apiProductData);
+                })
+            })
+        })
+    })
 });
